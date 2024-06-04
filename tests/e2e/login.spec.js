75daf0b1d90985a23298a06b6fe96cfd859eea98
alaninfo23 <js-alan@hotmail.com>
2024-06-04 10:40:32 -0300

    meu primeiro commit

---
+++ tests/e2e/login.spec.js
@@ -0,0 +1,39 @@
+const { test } = require('../support')
+
+test('deve logar como administrador', async ({ page }) => {
+    await page.login.visit()
+    await page.login.submit('admin@zombieplus.com', 'pwd123')
+    await page.movies.isLoggedIn()
+})
+
+test('não deve logar com senha incorreta', async ({ page }) => {
+    await page.login.visit()
+    await page.login.submit('admin@zombieplus.com', 'pwd1231')
+
+    const message = 'Oops!Ocorreu um erro ao tentar efetuar o login. Por favor, verifique suas credenciais e tente novamente.'
+    await page.toast.containText(message)
+})
+
+test('não deve logar quando o e-mail inválido', async ({ page }) => {
+    await page.login.visit()
+    await page.login.submit('papito@@hotmail.com', 'pwd1231')
+    await page.login.alertHaveText('Email incorreto')
+})
+
+test('não deve logar quando o e-mail não é preenchido', async ({ page }) => {
+    await page.login.visit()
+    await page.login.submit('', 'pwd1231')
+    await page.login.alertHaveText('Campo obrigatório')
+})
+
+test('não deve logar quando a senha não é preenchido', async ({ page }) => {
+    await page.login.visit()
+    await page.login.submit('admin@zombieplus.com', '')
+    await page.login.alertHaveText('Campo obrigatório')
+})
+
+test('não deve logar quando nenhum campo é preenchido', async ({ page }) => {
+    await page.login.visit()
+    await page.login.submit('', '')
+    await page.login.alertHaveText(['Campo obrigatório', 'Campo obrigatório'])
+})
