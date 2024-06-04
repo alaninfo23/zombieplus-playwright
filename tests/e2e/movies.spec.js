c48bdec9a183da223e98922b0339ceaaea6016dd
alaninfo23 <js-alan@hotmail.com>
2024-06-04 15:10:20 -0300

    corrigindo bugs no teste

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -4,6 +4,7 @@ const data = require('../support/fixtures/movies.json')
 const { executeSQL } = require('../support/database')
 
 test('deve poder cadastrar um novo filme', async ({ page }) => {
+
     const movie = data.create
     await executeSQL(`DELETE FROM movies WHERE title = '${movie.title}'`)
 
@@ -12,4 +13,18 @@ test('deve poder cadastrar um novo filme', async ({ page }) => {
     await page.movies.isLoggedIn()
     await page.movies.create(movie.title, movie.overview, movie.company, movie.release_year)
     await page.toast.containText('Cadastro realizado com sucesso!')
+})
+
+test('não deve cadastrar os campos obrigatórios não são preenchidos', async ({ page }) => {
+    await page.login.visit()
+    await page.login.submit('admin@zombieplus.com', 'pwd123')
+    await page.movies.isLoggedIn()
+    await page.movies.goForm()
+    await page.movies.submit()
+    await page.movies.alertHaveText([
+        'Por favor, informe o título.',
+        'Por favor, informe a sinopse.',
+        'Por favor, informe a empresa distribuidora.',
+        'Por favor, informe o ano de lançamento.'
+    ])
 })
--- tests/pages/MoviesPage.js
+++ tests/pages/MoviesPage.js
@@ -11,8 +11,17 @@ export class MoviesPage {
         await expect(this.page).toHaveURL(/.*admin/)
     }
 
-    async create(title, overview, company, release_year) {
+    async goForm() {
         await this.page.locator('a[href$="register"]').click()
+    }
+
+    async submit() {
+        await this.page.getByRole('button', { name: 'Cadastrar' }).click()
+
+    }
+
+    async create(title, overview, company, release_year) {
+        await this.goForm()
         await this.page.getByLabel('Titulo do filme').fill(title)
         await this.page.getByLabel('Sinopse').fill(overview)
         await this.page.locator('#select_company_id .react-select__indicator')
@@ -29,6 +38,10 @@ export class MoviesPage {
             .filter({ hasText: release_year })
             .click()
 
-        await this.page.getByRole('button', { name: 'Cadastrar' }).click()
+        await this.submit()
+    }
+
+    async alertHaveText(target) {
+        await expect(this.page.locator('.alert')).toHaveText(target)
     }
 }
